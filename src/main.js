45c39c634064eefe05dd60e0e29154cc863c6439
2107836160 <2107836160@qq.com>
2018-11-05 21:57:26 +0800

    '首页验证token-退出'

--- src/main.js
+++ src/main.js
@@ -3,9 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import HttpTool from '@/plugins/http.js'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import '@/assets/css/index.css'
 Vue.config.productionTip = false
 
+Vue.use(HttpTool)
+Vue.use(ElementUI)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
+
+import Login from '@/views/login.vue'
+
+import Home from '@/views/home.vue'
+
 import Router from 'vue-router'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: []
+  routes: [{
+    name: 'home',
+    path: '/',
+    component: Home
+  },{
+    name: 'login',
+    path: '/login',
+    component: Login
+  }]
 })
